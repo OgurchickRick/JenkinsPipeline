9ad5252e1e7a09c8facac0cfe57f524e085d6c6f
OgurchickRick <artem.derbin.04@bk.ru>
2024-08-03 16:55:24 +0300

    test

--- src/TelegramSendMessage.groovy
+++ src/TelegramSendMessage.groovy
@@ -1,28 +1,35 @@
-def sendTelegramNotification(status) {
-    withCredentials([string(credentialsId: 'telegram_bot_token', variable: 'BOT_TOKEN')]) {
-        def startTime = env.START_TIME ?: 'null'
-        def endTime = env.END_TIME ?: 'null'
-
-        sh """
-          curl -s -X POST https://api.telegram.org/bot${BOT_TOKEN}/sendMessage \
-               -d chat_id=-4286525343 \
-               -d parse_mode=MarkdownV2 \
-               -d text="*Performance Result*\n\n\
-\\- *Job Name:* ${env.JOB_NAME}\n\
-\\- *Status:* ${status}\n\
-\\- *Build Number:* ${env.BUILD_NUMBER}\n\
-\\- *Start Time:* ${startTime}\n\
-\\- *End Time:* ${endTime}" \
-        """
-    }
+def sendTelegramNotification(message) {
+    // Реализация отправки сообщения в Telegram
+    def token = 'YOUR_BOT_TOKEN'
+    def chatId = 'YOUR_CHAT_ID'
+    def url = "https://api.telegram.org/bot${token}/sendMessage"
+    def payload = [
+            chat_id: chatId,
+            text: message
+    ]
+    def response = httpRequest(
+            url: url,
+            httpMode: 'POST',
+            requestBody: new groovy.json.JsonBuilder(payload).toPrettyString(),
+            contentType: 'APPLICATION_JSON'
+    )
+    echo "Telegram response: ${response}"
 }
 
 def sendTelegramDocument() {
-    withCredentials([string(credentialsId: 'telegram_bot_token', variable: 'BOT_TOKEN')]) {
-        sh """
-          curl -v -X POST https://api.telegram.org/bot${BOT_TOKEN}/sendDocument \
-               -F chat_id=-4286525343 \
-               -F document=@example.tar
-        """
-    }
+    // Реализация отправки документа в Telegram
+    def token = 'YOUR_BOT_TOKEN'
+    def chatId = 'YOUR_CHAT_ID'
+    def url = "https://api.telegram.org/bot${token}/sendDocument"
+    def payload = [
+            chat_id: chatId
+            // Здесь добавьте необходимые параметры для отправки документа
+    ]
+    def response = httpRequest(
+            url: url,
+            httpMode: 'POST',
+            requestBody: new groovy.json.JsonBuilder(payload).toPrettyString(),
+            contentType: 'APPLICATION_JSON'
+    )
+    echo "Telegram response: ${response}"
 }
